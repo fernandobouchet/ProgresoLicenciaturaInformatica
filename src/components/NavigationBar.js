36a3e74bd6c6fba6c66d9afa82950e164629dcd7
fernandobouchet <fernandobouchet@gmail.com>
2022-06-12 16:43:39 -0300

    Style fixes to NavBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -15,7 +15,7 @@ const NavigationBar = (props) => {
 
   return (
     <>
-      <Navbar expand={'sm'} variant={theme} collapseOnSelect>
+      <StyledNavBar expand={'sm'} variant={theme} collapseOnSelect>
         <Container fluid>
           <Navbar.Brand as={Link} to={'/'}>
             <img
@@ -31,9 +31,6 @@ const NavigationBar = (props) => {
             id={`offcanvasNavbar-expand-${'sm'}`}
             aria-labelledby={`offcanvasNavbarLabel-expand-${'sm'}`}
             placement="end"
-            style={{
-              width: '60%',
-            }}
           >
             <StyledOffcanvasHeader closeButton fill={' '}>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'sm'}`}>
@@ -74,7 +71,7 @@ const NavigationBar = (props) => {
             </StyledOffcanvasBody>
           </Navbar.Offcanvas>
         </Container>
-      </Navbar>
+      </StyledNavBar>
       <ResetButtonContainer>
         <ResetButton
           show={modalShow}
@@ -87,6 +84,12 @@ const NavigationBar = (props) => {
 };
 
 export default NavigationBar;
+
+const StyledNavBar = styled(Navbar)`
+  .navbar-toggler {
+    border: none;
+  }
+`;
 
 const ResetButtonContainer = styled(Container)`
   position: absolute;
@@ -117,7 +120,7 @@ const StyledOffcanvasHeader = styled(Offcanvas.Header)`
       props.theme.text !== 'black' &&
       `background: transparent
         url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='white'%3e%3cpath d='M.293.293a1 1 0 011.414 0L8 6.586 14.293.293a1 1 0 111.414 1.414L9.414 8l6.293 6.293a1 1 0 01-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 01-1.414-1.414L6.586 8 .293 1.707a1 1 0 010-1.414z'/%3e%3c/svg%3e")
-        center/1em auto no-repeat;`}
+        center/1.2em auto no-repeat;`}
   }
 `;
 
@@ -126,5 +129,18 @@ const StyledOffcanvasBody = styled(Offcanvas.Body)`
   color: ${(props) => props.theme.text};
   a {
     color: ${(props) => props.theme.text};
+  }
+  .navbar-nav {
+    width: 50%;
+    margin: auto;
+  }
+  .nav-item {
+    .dropdown-menu {
+      border: 1px solid ${(props) => props.theme.border};
+      text-align: center;
+      hr {
+        color: ${(props) => props.theme.text};
+      }
+    }
   }
 `;
